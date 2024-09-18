a1c9b067bb54441604ce192ed6d1e2d1da0ceda5
Hsieh Nicolas Federico <hsiehnicolas@gmail.com>
2024-09-18 11:30:31 -0300

    Update game_company.sql

--- game_company.sql
+++ game_company.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-09-2024 a las 16:04:04
+-- Tiempo de generación: 18-09-2024 a las 16:29:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,23 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `company` (
-  `Company_ID` int(11) NOT NULL,
+  `Company_id` int(11) NOT NULL,
   `Company_name` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `game`
+-- Estructura de tabla para la tabla `games`
 --
 
-CREATE TABLE `game` (
+CREATE TABLE `games` (
   `Game_ID` int(11) NOT NULL,
   `Game_name` varchar(45) NOT NULL,
   `Genre` varchar(45) NOT NULL,
   `Year` int(11) NOT NULL,
   `Puntuation` int(11) NOT NULL,
-  `Company_ID` int(11) NOT NULL
+  `Company_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -55,30 +55,14 @@ CREATE TABLE `game` (
 -- Indices de la tabla `company`
 --
 ALTER TABLE `company`
-  ADD PRIMARY KEY (`Company_ID`);
+  ADD PRIMARY KEY (`Company_id`);
 
 --
--- Indices de la tabla `game`
+-- Indices de la tabla `games`
 --
-ALTER TABLE `game`
+ALTER TABLE `games`
   ADD PRIMARY KEY (`Game_ID`),
-  ADD UNIQUE KEY `oooo` (`Company_ID`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `company`
---
-ALTER TABLE `company`
-  MODIFY `Company_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `game`
---
-ALTER TABLE `game`
-  MODIFY `Game_ID` int(11) NOT NULL AUTO_INCREMENT;
+  ADD KEY `Company_id` (`Company_id`);
 
 --
 -- Restricciones para tablas volcadas
@@ -88,14 +72,13 @@ ALTER TABLE `game`
 -- Filtros para la tabla `company`
 --
 ALTER TABLE `company`
-  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`Company_ID`) REFERENCES `game` (`Company_ID`);
+  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`Company_id`) REFERENCES `games` (`Company_id`);
 
 --
--- Filtros para la tabla `game`
+-- Filtros para la tabla `games`
 --
-ALTER TABLE `game`
-  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`Company_ID`) REFERENCES `company` (`Company_ID`),
-  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`Game_ID`) REFERENCES `company` (`Company_ID`);
+ALTER TABLE `games`
+  ADD CONSTRAINT `games_ibfk_1` FOREIGN KEY (`Company_id`) REFERENCES `company` (`Company_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
